c8923a6de27cf1b148d2b4775c747e1b98c0c4dd
luluatina <luluatina770@gmail.com>
2025-09-01 16:30:57 +0700

    Add files via upload

---
+++ Lulu_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lulu_project1;
+
+/**
+ *
+ * @author user
+ */
+public class Lulu_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    :Lu'luatina Zakiyatal Fikri");
+        System.out.println("NPM     :2420506034");
+        System.out.println("Alamat  :Balekerto, Kaliangkrik, Magelang");
+        System.out.println("No HP   :085566778822");
+    }
+}
